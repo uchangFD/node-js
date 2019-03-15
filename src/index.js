b3c7b5af071a69120c11eb23a03076df8cb19b71
uchang heo <ucheo@rsupport.com>
2019-03-15 16:16:37 +0900

    RESTApi koa-router, koa-parser / vscode settings(prettier, eslint)

--- src/index.js
+++ src/index.js
@@ -1,30 +1,16 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const api = require('./api');
 
 const app = new Koa();
 const router = new Router();
 
+router.use('/api', api.routes());
+
+app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
-
-router.get('/', (ctx) => {
-  ctx.body = '홈';
-});
-
-router.get('/about/:name', (ctx) => {
-  const { name } = ctx.params;
-  ctx.body = `${name}의 소개`;
-});
-
-router.get('/posts', (ctx) => {
-  const { id } = ctx.query;
-  if (id) {
-    ctx.body = `포스트 #${id}`;
-    return;
-  }
-
-  ctx.body = '선택된 포스트가 없습니다.';
-});
 
 app.listen(4000, () => {
   console.log('Server is listening to port 4000');
